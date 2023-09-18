41a3486b40db4ca29e688713234797f858f60def
Ed Stokes <edstokes96@gmail.com>
2023-09-18 12:41:29 -0500

    Added title for Home page and tech list

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,6 +1,39 @@
+import React, {useEffect, useState} from "react";
+
+function TechList() {
+    const [techs, setTechs] = useState([]);
+    useEffect(() => {
+        fetch("http://localhost:4000/techs")
+        .then((response) => response.json())
+        .then((techs) => setTechs(techs));
+    }, []);
+
+    return (
+        <div className="techList"> 
+            <h2>STN1 RME TECHS</h2>
+            <ul>
+            {techs.map((tech) => (
+                <li key={tech.name}>{tech.name}</li>
+            ))}
+            </ul>
+        </div>
+    )
+}
+
+function ShiftInfo() {
+
+}
+
+
 function Home() {
     return (
-        <h1>Home Page</h1>
+        <div>
+        <h1 className="homeTitle">Welcome to STN1</h1>
+        <p>Serving the best of Nashville since 2020</p>
+        <TechList />
+        
+        </div>
+        
     )
 }
 
