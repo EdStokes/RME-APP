b4fdadb584965739c08ddde95888f53f62103473
Ed Stokes <edstokes96@gmail.com>
2023-09-18 08:52:28 -0500

    Correct error in NavBar and added routes to app

--- src/App.js
+++ src/App.js
@@ -1,9 +1,26 @@
+import {Route, Switch} from "react-router-dom";
+import NavBar from "./components/NavBar";
+import Home from "./components/Home";
+import Passdown from "./components/Passdown";
+import Safety from "./components/Safety";
+
 
 
 function App() {
   return (
-    <div className="App">
-     <h1>RME APP</h1>
+    <div>
+      <NavBar />
+      <Switch>
+        <Route exact path="/">
+          <Home />
+        </Route>
+        <Route exact path="/Passdown">
+          <Passdown />
+        </Route>
+        <Route exact path="/Safety">
+          <Safety />
+        </Route>
+      </Switch>
     </div>
   );
 }
--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -4,9 +4,9 @@ function NavBar() {
 
     return (
         <div>
-            <NavLink to="/" exact></NavLink>
+            <NavLink to="/" exact>Home</NavLink>
             <NavLink to="/Passdown" exact>Passdown</NavLink>
-            <NavLink to="/Safety" exact></NavLink>
+            <NavLink to="/Safety" exact>Safety</NavLink>
         </div>
     )
 }
