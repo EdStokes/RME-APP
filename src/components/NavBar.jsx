3f21576bc88f203cf621096da147aadb8258484f
Ed Stokes <edstokes96@gmail.com>
2023-09-18 10:13:27 -0500

    Added CSS file and formated top nav bar

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,10 +1,12 @@
 import { NavLink } from "react-router-dom";
+import "../styles.css";
 
 function NavBar() {
 
     return (
-        <div>
-            <NavLink to="/" exact>Home</NavLink>
+        <div className="navBar">
+            <h2>STN1 RME</h2>
+            <NavLink to="/" exact className="homeLink">Home</NavLink>
             <NavLink to="/Passdown" exact>Passdown</NavLink>
             <NavLink to="/Safety" exact>Safety</NavLink>
         </div>
