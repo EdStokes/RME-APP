1d146a80e19366bb46ca30c70efb0526e99b223c
Ed Stokes <edstokes96@gmail.com>
2023-09-19 03:47:43 -0500

    Added function in Passdown component to change background color based on status

--- src/components/Passdown.jsx
+++ src/components/Passdown.jsx
@@ -2,41 +2,49 @@ import React, { useEffect, useState } from "react";
 
 function PassdownHeader({ techs }) {
     return (
-    <div className="passdownHeader">
-        <form>
-            <label>Tech</label>
-            <select>
-                {techs.map((tech) => (
-                    <option>{tech.name}</option>
-                ))}
-            </select>
-           <label>Date: </label>
-           <input type="date" />
-           <lable>Time In: </lable>
-           <input type="time" />
-           <label>Time Out: </label>
-           <input type="time" />
-           <label>Hours Worked: </label>
-           <input type="text" />
-           <label>95% Hours: </label>
-           <input type="text" />
-           <label>Booked: </label>
-           <input types="text" />
-           <button>Add Work</button>
-        </form>
-    </div>
+        <div className="passdownHeader">
+            <form>
+                <label>Tech</label>
+                <select>
+                    {techs.map((tech) => (
+                        <option>{tech.name}</option>
+                    ))}
+                </select>
+                <label>Date: </label>
+                <input type="date" />
+                <lable>Time In: </lable>
+                <input type="time" />
+                <label>Time Out: </label>
+                <input type="time" />
+                <label>Hours Worked: </label>
+                <input type="text" />
+                <label>95% Hours: </label>
+                <input type="text" />
+                <label>Booked: </label>
+                <input types="text" />
+                <button>Add Work</button>
+            </form>
+        </div>
     );
 }
 
-        function PassdownInfo() {
+function statusColor(status) {
+    if (status === "Completed") {
+        return "green"
+    }else if (status === "In-Progress") {
+        return "yellow"
+    }
+}
+
+function PassdownInfo() {
     const [currentPassdownInfo, setCurrentPassdownInfo] = useState([]);
     useEffect(() => {
-            fetch("http://localhost:4000/passdowns")
-                .then((response) => response.json())
-                .then((info) => setCurrentPassdownInfo(info))
-        }, []);
+        fetch("http://localhost:4000/passdowns")
+            .then((response) => response.json())
+            .then((info) => setCurrentPassdownInfo(info))
+    }, []);
 
-        return (
+    return (
         <div>
             <table className="passdownTable">
                 <thead>
@@ -54,24 +62,24 @@ function PassdownHeader({ techs }) {
                             <td>{info.workorder}</td>
                             <td>{info.description}</td>
                             <td>{info.bookedLabor}</td>
-                            <td>{info.status}</td>
+                            <td style={{backgroundColor: statusColor(info.status)}}>{info.status}</td>
                             <td>{info.comments}</td>
                         </tr>
                     ))}
                 </tbody>
             </table>
         </div>
-        )
+    )
 }
 
-        function Passdown({techs}) {
+function Passdown({ techs }) {
 
     return (
         <div>
             <PassdownHeader techs={techs} />
             <PassdownInfo />
         </div>
-        )
+    )
 }
 
-        export default Passdown;
+export default Passdown;
