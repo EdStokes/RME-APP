c592b3f9d1285a9517eb3b53cdf9c4d093a1a0e5
Ed Stokes <edstokes96@gmail.com>
2023-09-24 10:47:26 -0500

    Added new formating to css for navBar

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -6,11 +6,15 @@ function NavBar() {
 
     return (
         <div className="navBar">
-            <img src={logo} alt='Site Logo' className="navLogo" />
-            <h2>STN1 RME</h2>
-            <NavLink to="/" exact className="homeLink">Home</NavLink>
-            <NavLink to="/Passdown" exact className="passdownLink">Passdown</NavLink>
-            <NavLink to="/Safety" exact>Safety</NavLink>
+            <div className="navBarLeft">
+                <img src={logo} alt='Site Logo' className="navLogo" />
+                <h2>STN1 RME</h2>
+            </div>
+            <div className="navBarRight">
+                <NavLink to="/" exact className="homeLink">Home</NavLink>
+                <NavLink to="/Passdown" exact className="passdownLink">Passdown</NavLink>
+                <NavLink to="/Safety" exact className="safetyLink">Safety</NavLink>
+            </div>
         </div>
     )
 }
