b517833f487bafb90c2a5ce936157d63c0934491
Ed Stokes <edstokes96@gmail.com>
2023-09-26 00:10:07 -0500

    Added form to createPassdown.jsx

--- src/components/CreatePassdown.jsx
+++ src/components/CreatePassdown.jsx
@@ -1,62 +1,58 @@
-import React, {useEffect, useState} from "react";
 
 
-function statusColor(status) {
-    if (status === "Completed") {
-        return "green"
-    } else if (status === "In-Progress") {
-        return "yellow"
-    }
+import React, { useState } from "react";
+import {useHistory} from "react-router-dom"
+
+function PassdownForm() {
+    const [selectedTech, setSelectedTech] = useState("");
+    const [date, setDate] = useState("");
+    const [timeIn, setTimeIn] = useState("");
+    const [timeOut, setTimeOut] = useState("");
+    const history = useHistory();
+
+    const handleSubmit = (event) => {
+        event.preventDefault();
+
+        const passdownInfo = {
+            tech: selectedTech,
+            date,
+            timeIn,
+            timeOut,
+        };
+
+        // onSubmitPassdownInfo(passdownInfo);
+        // history.push("/Passdown")
+    };
+
+    return (
+        <div className="passdownHeader">
+            <form onSubmit={handleSubmit}>
+                <div className="passdownHeaderTop">
+                    <label>Tech</label>
+                    {/* <select value={selectedTech} onChange={(event) => setSelectedTech(event.target.value)}>
+                        {techs.map((tech) => (
+                            <option key={tech.name}>{tech.name}</option>
+                        ))}
+                    </select> */}
+                    <label>Date: </label>
+                    <input type="date" value={date} onChange={(event) => setDate(event.target.value)} />
+                    <label>Time In: </label>
+                    <input type="time" value={timeIn} onChange={(event) => setTimeIn(event.target.value)} />
+                    <label>Time Out: </label>
+                    <input type="time" value={timeOut} onChange={(event) => setTimeOut(event.target.value)} />
+                </div>
+                <button type="submit">Submit</button>
+            </form>
+        </div>
+    );
+
 }
 
-function RenderPassdown(){
-    const [currentPassdownInfo, setCurrentPassdownInfo] = useState([]);
-
-    useEffect(() => {
-        fetch("http://localhost:4000/passdowns")
-            .then((response) => response.json())
-            .then((data) => {
-                setCurrentPassdownInfo(data);
-            });
-}, []);
-
-return ( 
-    <div>
-
-        <table className="passdownTable">
-            <thead>
-                <tr>
-                    <th>WO#</th>
-                    <th>Description</th>
-                    <th>Booked Labor</th>
-                    <th>Status</th>
-                    <th>Comments</th>
-                </tr>
-            </thead>
-            <tbody>
-                {currentPassdownInfo.map((info) => (
-                    <tr key={info.id}>
-                        {info.workorder.map((wo, index) => (
-                            <React.Fragment key={`${info.id}-${index}`}>
-                                <td>{wo.workorder}</td>
-                                <td>{wo.description}</td>
-                                <td>{wo.bookedLabor}</td>
-                                <td style={{backgroundColor: statusColor(wo.status)}}>{wo.status}</td>
-                                <td>{wo.comments}</td>
-                            </React.Fragment>
-                        ))}  
-                    </tr>
-                ))}
-            </tbody>
-        </table>
-    </div>
-)
-}
 
 
 function CreatePassdown() {
     return (
-        <RenderPassdown />
+        <PassdownForm />
     )
 }
 
